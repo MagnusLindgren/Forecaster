9efb10b985c4991e43018a8413b284b3137a36b2
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-04 09:28:10 +0100

    Lagt till lite JS. Inget färdigt

--- js/main.js
+++ js/main.js
@@ -2,4 +2,25 @@
     * Fix the API
     * search function
     * make checkboxes work
+    * Function that deletes previous results
 */
+document.addEventListener("DOMContentLoaded", pageLoaded);
+
+"useStrict";
+
+function pageLoaded() {
+    let searchBox = document.getElementById("#searchBox");
+
+    
+}
+
+function searchAPI(searchWord) {
+
+}
+
+function createSearchResult(count) {
+    for (let i = 0; i < count; i++) {
+        let parent = document.createElement("section");
+        let child = document.createElement("div");
+    }
+}

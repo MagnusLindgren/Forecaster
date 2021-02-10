1f6a2c8de22b305d446e5691016d2666db11ad4d
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-10 16:44:05 +0100

    Rensat och gjort renare URLer

--- js/main.js
+++ js/main.js
@@ -9,25 +9,54 @@ document.addEventListener("DOMContentLoaded", pageLoaded);
 
 function pageLoaded() {
     /* Variables */
+    /*
     const CLIENT_ID = "UT23XCWOFEUB3EXA40EQXRVCLN1NTE1XEJLS1JUNJAWQFSYV";
     const CLIENT_SECRET = "WSOBYLFKSXJAJYWV1MGFW45RPIY0DRI3YDC0I0EDRRLEDTEM";
-    const SEARCHBUTTON = document.getElementById('searchButton');  
+    const API_KEY = '984e8b07157055164de5d508d1e7e094';
+    let weatherApi, mode, units, lang, url;
+    weatherApi = 'https://api.openweathermap.org/data/2.5/weather';
+    mode = 'json';
+    units = 'metric';
+    lang = 'se';  
     let today = dateBuilder();
+    let venueApi = 'https://api.foursquare.com/v2/venues/explore';
+    let jsonResponse;
+    */
 
+   const SEARCHBUTTON = document.getElementById('searchButton');
     SEARCHBUTTON.onclick = function() {
-        let searchTerm = document.getElementById('searchBox').value;
-        console.log("Search value = " + searchTerm);
-        url = new URL(`${url['venueApi']}?near=${searchTerm}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}&limit=10`);
-        let jsonResponse = fetchVenueApi(url);
-        let city = jsonResponse._response.headerFullLocation;
-        createData(city);
-    }
+        let searchTerm = 'helsingborg'; //document.getElementById('searchBox').value;
+        console.log(`Search value: ${searchTerm}`);
+        //venueUrl = new URL(`${venueApi}?near=${searchTerm}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}&limit=10`);
 
+        jsonResponse = fetchApi(getVenueApi(searchTerm)).then(result => console.log(result));
+        //jsonResponse2 = fetchApi(weatherUrl).then(result => console.log(result));  
+        //createData(jsonResponse);   
+    }
 }
 
-let url = {
-    'venueApi': 'https://api.foursquare.com/v2/venues/explore',
-    'weatherApi': 'https://api.openweathermap.org/data/2.5/weather'
+function getWeatherApi(city) {
+    const weatherUrl = new URL('https://api.openweathermap.org/data/2.5/weather');
+
+    weatherUrl.searchParams.append('q', city);
+    weatherUrl.searchParams.append('appid', '984e8b07157055164de5d508d1e7e094');
+    weatherUrl.searchParams.append('mode', 'json');
+    weatherUrl.searchParams.append('units', 'metric');
+    weatherUrl.searchParams.append('lang', 'se');
+
+    return weatherUrl;
+}
+
+function getVenueApi(city) {
+    const venueUrl = new URL('https://api.foursquare.com/v2/venues/explore');
+    const today = dateBuilder();
+
+    venueUrl.searchParams.append('near', city);
+    venueUrl.searchParams.append('client_id', 'UT23XCWOFEUB3EXA40EQXRVCLN1NTE1XEJLS1JUNJAWQFSYV');
+    venueUrl.searchParams.append('client_secret', 'WSOBYLFKSXJAJYWV1MGFW45RPIY0DRI3YDC0I0EDRRLEDTEM');
+    venueUrl.searchParams.append('v', today);
+
+    return venueUrl;
 }
 
 /* Funktion för att skapa datum som jag skapade för att förenkla url:en ytterligare*/
@@ -49,13 +78,12 @@ function dateBuilder() {
     return todaysDate;
 }
 
-async function fetchVenueApi(url) {
+/* Hämtar API utifrån en URL*/
+async function fetchApi(url) {
     try {
         let response = await fetch(url);
         if (response.ok) {
             let jsonResponse = await response.json();
-            console.log(`Request status: ${jsonResponse.meta.code}`);
-            console.log(jsonResponse);
             return jsonResponse;
         } else {
             console.log(url + " not loaded successfully");
@@ -65,9 +93,9 @@ async function fetchVenueApi(url) {
     }    
 }
 
-function createData(city) {
+function createData(city) {    
     let cityName = document.querySelector("#cityName");
-    cityName.innerText = city;
+    cityName.innerText = city.meta.response.response.headerLocation;
 }
 
 /* For future 

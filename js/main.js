86bd45303cd67c765693e95058078eb43bee016b
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-10 11:53:04 +0100

    Massor av expriment

--- js/main.js
+++ js/main.js
@@ -11,18 +11,23 @@ function pageLoaded() {
     /* Variables */
     const CLIENT_ID = "UT23XCWOFEUB3EXA40EQXRVCLN1NTE1XEJLS1JUNJAWQFSYV";
     const CLIENT_SECRET = "WSOBYLFKSXJAJYWV1MGFW45RPIY0DRI3YDC0I0EDRRLEDTEM";
-    const SEARCHBUTTON = document.getElementById('searchButton');
-    let cityUrl = 'https://api.foursquare.com/v2/venues/explore';   
+    const SEARCHBUTTON = document.getElementById('searchButton');  
     let today = dateBuilder();
 
     SEARCHBUTTON.onclick = function() {
         let searchTerm = document.getElementById('searchBox').value;
         console.log("Search value = " + searchTerm);
-        url = new URL(`${cityUrl}?near=${searchTerm}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}&limit=10`);
-        fetchAPI(url)
-            .then(result => console.log(result))
+        url = new URL(`${url['venueApi']}?near=${searchTerm}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}&limit=10`);
+        let jsonResponse = fetchVenueApi(url);
+        let city = jsonResponse._response.headerFullLocation;
+        createData(city);
     }
 
+}
+
+let url = {
+    'venueApi': 'https://api.foursquare.com/v2/venues/explore',
+    'weatherApi': 'https://api.openweathermap.org/data/2.5/weather'
 }
 
 /* Funktion för att skapa datum som jag skapade för att förenkla url:en ytterligare*/
@@ -44,20 +49,25 @@ function dateBuilder() {
     return todaysDate;
 }
 
-async function fetchAPI(url) {
+async function fetchVenueApi(url) {
     try {
         let response = await fetch(url);
         if (response.ok) {
             let jsonResponse = await response.json();
-            console.log(url + " loaded successfully");
+            console.log(`Request status: ${jsonResponse.meta.code}`);
+            console.log(jsonResponse);
             return jsonResponse;
         } else {
             console.log(url + " not loaded successfully");
         }        
     } catch (error) {
             console.log("Error");
-    }
-    
+    }    
+}
+
+function createData(city) {
+    let cityName = document.querySelector("#cityName");
+    cityName.innerText = city;
 }
 
 /* For future 
